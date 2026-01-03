ff8a1ea8f316c1806f8dbd744917f5654584e39b
septiantian17092006-del <septiantian17092006@gmail.com>
2026-01-03 14:49:47 +0800

    latihan7
    uts3

---
+++ main7.py
@@ -0,0 +1,8 @@
+from utils import konversi_angka
+
+# Contoh angka
+angka = 5.548363
+
+# Menampilkan hasil
+print("Angka awal :", str(angka).replace(".", ","))
+print("Hasil konversi :", konversi_angka(angka))
