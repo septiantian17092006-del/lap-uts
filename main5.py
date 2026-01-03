db5d917f47537ab5ed0bf81859ac68966c80986f
septiantian17092006-del <septiantian17092006@gmail.com>
2026-01-03 14:48:53 +0800

    latihan5
    uts3

---
+++ main5.py
@@ -0,0 +1,7 @@
+from utils import format_rupiah
+
+# Input angka dari pengguna
+angka = int(input("Masukkan angka: "))
+
+# Menampilkan hasil dalam format Rupiah
+print("Hasil:", format_rupiah(angka))
